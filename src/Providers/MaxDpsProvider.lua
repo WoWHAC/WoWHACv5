8f6212f00b99cd78938a17d4518ad01a9628165b
CKATEPTb <ckateptb@vivaldi.net>
2026-04-12 15:06:56 +0200

    Refactor MaxDps provider integration

--- src/Providers/MaxDpsProvider.lua
+++ src/Providers/MaxDpsProvider.lua
@@ -1,24 +1,22 @@
 local _, WoWHACv5 = ...
-
 WoWHACv5.providers = WoWHACv5.providers or {}
 WoWHACv5.providers["MaxDps"] = function()
     WoWHACv5:Log("Supplier found: MaxDps.")
     if not WeakAuras then
-        function WoWHACv5:GetCurrentHotKey()
-            for _, frame in pairs(MaxDps.Frames) do
-                if frame and frame:IsVisible() then
-                    if WoWHACv5.burst or frame.ovType ~= "cooldown" then
-                        local button = frame:GetParent()
-                        return button.HotKey:GetText()
-                    end
-                end
-            end
-        end
+        MaxDps.db.global.onCombatEnter = false
+        MaxDps.db.global.spellFrame.enabled = true
+        MaxDps.db.global.enabled = true
+        MaxDpsSpellFrame:Show()
 
-        function WoWHACv5:GetCurrentId()
-            return MaxDps.Spell
-        end
+        local provider = MaxDpsSpellFrame
+        local Keybind = provider.bindText
+
+        WoWHACv5:SecureHook(MaxDps, "UpdateSpellFrame", function(_, spellID)
+            WoWHACv5:SetCurrentId(spellID)
+            WoWHACv5:SetCurrentHotKey(spellID and spellID > 0 and Keybind:GetText() or nil)
+        end)
     else
+--         Это нужно сохранить, это поддержка legacy версий игры
         WoWHACv5:RegisterMessage("WOWHACV4_WA_PRESENTS", function(_, _, isLoaded)
             if isLoaded then
                 WoWHACv5.ToggleBurstFrame:Show()
